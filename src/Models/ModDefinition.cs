d454ade5ba65911127b8858eea2485204d133045
SkyCopeland <sky@skycopeland.com>
2024-12-09 01:57:19 -0500

    Improved Support for Generic JSONs [skip ci]

--- src/Models/ModDefinition.cs
+++ src/Models/ModDefinition.cs
@@ -4,7 +4,7 @@ public class ModDefinition
 {
     public string? Id { get; set; }
     public string? Name { get; set; }
-    public string? Description { get; set; }
     public string? IconUrl { get; set; }
     public string? ThunderstoreUrl { get; set; }
+    public bool IsDynamicMod { get; set; } = false;
 }
